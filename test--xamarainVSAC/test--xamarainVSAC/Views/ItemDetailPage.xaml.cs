714e4e9e66a0163ee16fc5c1fef56ef2f671376a
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-02-01 13:53:04 +0430

    Add project files.

---
+++ test--xamarainVSAC/test--xamarainVSAC/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using test__xamarainVSAC.ViewModels;
+
+namespace test__xamarainVSAC.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
